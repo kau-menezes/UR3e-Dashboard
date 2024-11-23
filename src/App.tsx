7ceff47ff4a1dde9dc0267836c8585ec4d431f6d
kau-menezes <kau.vmenezes@gmail.com>
2024-11-23 16:59:08 -0300

    feat: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { RouterProvider } from "react-router-dom"
 import "./styles/global.css"
+import router from "./routes/router"
 
 
 export default function App() {
     return (
         <>
-          
+            <RouterProvider router={router}/>
         </>
     )
 }
