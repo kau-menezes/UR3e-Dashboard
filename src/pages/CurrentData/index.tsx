f84ba6870529a4aae92ec3dcb0c2e10566008598
kau-menezes <kau.vmenezes@gmail.com>
2024-11-23 17:41:42 -0300

    database connection

--- src/pages/CurrentData/index.tsx
+++ src/pages/CurrentData/index.tsx
@@ -1,5 +1,23 @@
+import { onValue, ref } from "firebase/database";
+import { database } from "../../service/firebase.services";
+import { useEffect, useState } from "react";
+
 export default function CurrentData() {
+
+    const [data, setData] = useState<any>({})
+
+    const dataRef = ref(database, "lastData")
+
+    useEffect(() => {
+        return onValue(dataRef, (snapshot) => {
+            const query = snapshot.val()
+            setData(query)
+        })
+    }, [])
+
     return(
-        <></>
+        <>
+            
+        </>
     )
 }
