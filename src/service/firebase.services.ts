5e3671b404938d011aa4210fc32f308ebb1011ee
kau-menezes <kau.vmenezes@gmail.com>
2024-11-23 17:14:56 -0300

    config: firebase

---
+++ src/service/firebase.services.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDmJLy9G9E6UGmJTy-2dfXJfRB8Kz1Jqtw",
+    authDomain: "ur3e-dashboard.firebaseapp.com",
+    databaseURL: "https://ur3e-dashboard-default-rtdb.firebaseio.com",
+    projectId: "ur3e-dashboard",
+    storageBucket: "ur3e-dashboard.firebasestorage.app",
+    messagingSenderId: "803272173220",
+    appId: "1:803272173220:web:e3848ddb46c0c6c519e831",
+    measurementId: "G-6TRXYCXYJL"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
